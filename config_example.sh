2a4f3bb46e5a7770bf4c5046096ff67f585959a0
Joao PN <joaoxp@gmail.com>
2023-04-23 14:26:28 +0200

    adds config_example.sh

---
+++ config_example.sh
@@ -0,0 +1,11 @@
+# SendGrid API key
+SENDGRID_API_KEY="your_sendgrid_key"
+
+# Email recipients and senders
+TO_EMAIL="your_email"
+TO_NAME="your_name"
+FROM_EMAIL="your_email"
+FROM_NAME="your_name"
+
+# Error log file
+ERROR_LOG="error_scripts.log"
